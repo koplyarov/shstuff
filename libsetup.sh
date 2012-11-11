d7dd4fecdd4f97366b2ee7f7160988a4a47a0e11
Dmitry Koplyarov <koplyarov.da@gmail.com>
2012-11-11 05:46:17 +0400

    First libsetup version

---
+++ libsetup.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+. "`dirname $0`/toolkit.sh"
+
+Exec() {
+	local IFS=$' '
+	local MSG=`eval msg_$ACTION`
+	Log Log_NoLineFeed "$MSG..."
+	__LOCAL_OUT=`eval do_$ACTION 2>&1`
+	if [ $? -eq 0 ]; then
+		Log Log_NoPreamble Log_ColoredMsg "OK"
+		return 0
+	else
+		Log Error Log_NoPreamble Log_ColoredMsg "Fail!"
+		Log Error "$__LOCAL_OUT"
+		return 1
+	fi
+}
+
+Unwind() {
+	local IFS=$' '
+	local ACTION="$@"
+	local MSG=`eval msg_$ACTION`
+	Log Log_NoLineFeed "Unwinding $ACTION..."
+	__LOCAL_OUT=`eval undo_$ACTION 2>&1`
+	if [ $? -eq 0 ]; then
+		Log Log_NoPreamble Log_ColoredMsg " OK"
+	else
+		Log Log_NoPreamble Log_ColoredMsg Warning " Fail!"
+		Log Warning "$__LOCAL_OUT"
+	fi
+}
+
+Install() {
+	local ACTIONS="$@"
+	local IFS=$'\n'
+	for ACTION in $ACTIONS; do
+		Exec "$ACTION"
+		if [ $? -ne 0 ]; then
+			local IFS=$'\n'
+			for UNWIND_ACTION in $UNWIND_ACTIONS; do
+				Unwind "$UNWIND_ACTION"
+			done
+			break
+		fi
+		local UNWIND_ACTIONS="$ACTION
+$UNWIND_ACTIONS"
+	done
+}
+
+Uninstall() {
+	local ACTIONS="$@"
+	local IFS=$'\n'
+	for ACTION in $ACTIONS; do
+		local UNWIND_ACTIONS="$ACTION
+$UNWIND_ACTIONS"
+	done
+
+	local IFS=$'\n'
+	for UNWIND_ACTION in $UNWIND_ACTIONS; do
+		Unwind "$UNWIND_ACTION"
+	done
+}
+
+AddAction() {
+	V=$1
+	shift
+	eval __LOCAL_OLDVAL="\$$V"
+	[ -z "$__LOCAL_OLDVAL" ] || eval $V="\$${V}\"$LINEFEED\""
+	eval $V="\$${V}\$1"
+	shift
+	for ARG in "$@"; do
+		eval $V="\$${V}\ \\\"\$ARG\\\""
+	done;
+}
