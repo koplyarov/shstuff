51694adf16c8aab5fc1f964d6c4ce19456fc7566
Dmitry Koplyarov <koplyarov.da@gmail.com>
2013-01-08 23:04:53 +0400

    Setup params support

--- libsetup.sh
+++ libsetup.sh
@@ -3,50 +3,104 @@
 
 Exec() {
 	local IFS=" "
+	local SETUP_OBJ=$1
+	shift
+	local ACTION="$@"
 	local MSG=`eval msg_$ACTION`
-	Log Log_NoLineFeed "$MSG..."
-	__LOCAL_OUT=`eval do_$ACTION 2>&1`
-	if [ $? -eq 0 ]; then
-		Log Log_NoPreamble Log_ColoredMsg "OK"
-		return 0
+	if [ "`GetSetupParam $SETUP_OBJ Verbose`" -eq "0" ]; then
+		Log Log_NoLineFeed "$MSG..."
+		__LOCAL_OUT=`eval do_$ACTION 2>&1`
+		if [ $? -eq 0 ]; then
+			Log Log_NoPreamble Log_ColoredMsg "OK"
+			return 0
+		else
+			Log Error Log_NoPreamble Log_ColoredMsg "Fail!"
+			Log Error "$__LOCAL_OUT"
+			return 1
+		fi
 	else
-		Log Error Log_NoPreamble Log_ColoredMsg "Fail!"
-		Log Error "$__LOCAL_OUT"
-		return 1
+		Log "$MSG..."
+		eval do_$ACTION 2>&1
+		if [ $? -eq 0 ]; then
+			Log Log_ColoredMsg "OK"
+			return 0
+		else
+			Log Error Log_ColoredMsg "Fail!"
+			return 1
+		fi
 	fi
 }
 
 Revert() {
 	local IFS=" "
+	local SETUP_OBJ=$1
+	shift
 	local ACTION="$@"
 	local MSG=`eval msg_$ACTION`
-	Log Log_NoLineFeed "Reverting '$MSG'..."
-	__LOCAL_OUT=`eval undo_$ACTION 2>&1`
-	if [ $? -eq 0 ]; then
-		Log Log_NoPreamble Log_ColoredMsg " OK"
+	if [ "`GetSetupParam $SETUP_OBJ Verbose`" -eq "0" ]; then
+		Log Log_NoLineFeed "Reverting '$MSG'..."
+		__LOCAL_OUT=`eval undo_$ACTION 2>&1`
+		if [ $? -eq 0 ]; then
+			Log Log_NoPreamble Log_ColoredMsg "OK"
+		else
+			Log Warning Log_NoPreamble Log_ColoredMsg "Fail!"
+			Log Warning "$__LOCAL_OUT"
+		fi
 	else
-		Log Warning Log_NoPreamble Log_ColoredMsg " Fail!"
-		Log Warning "$__LOCAL_OUT"
+		Log "Reverting '$MSG'..."
+		eval undo_$ACTION 2>&1
+		if [ $? -eq 0 ]; then
+			Log Log_ColoredMsg "OK"
+		else
+			Log Warning Log_ColoredMsg "Fail!"
+		fi
 	fi
 }
 
 CreateSetup()
 {
-	V=$1
-	shift
-	eval $V=\"\"
+	local SETUP_OBJ=$1
+	eval $SETUP_OBJ=\"\"
+	SetSetupParam $SETUP_OBJ Verbose 0
+}
+
+SetSetupParam() {
+	if [ $# -le 2 ]; then
+		Log Error "Too few arguments for SetSetupParam!"
+		return 1
+	fi
+	case $2 in
+	"Verbose"|"User_"*)	local SETUP_PARAM=$1_PARAM_$2 ;;
+	*)					Log Error "Invalid setup parameter: $2"; return 2 ;;
+	esac
+	shift 2
+	eval $SETUP_PARAM=\""$@"\"
+}
+
+GetSetupParam() {
+	if [ $# -ne 2 ]; then
+		Log Error "Invalid number of arguments for GetSetupParam!"
+		return 1
+	fi
+	case $2 in
+	"Verbose"|"User_"*)	local SETUP_PARAM=$1_PARAM_$2 ;;
+	*)					Log Error "Invalid setup parameter: $2"; return 2 ;;
+	esac
+	shift 2
+	eval echo \"\$$SETUP_PARAM\"
 }
 
 Install() {
-	eval __LOCAL_ACTIONS="\$$1"
+	local SETUP_OBJ=$1
+	eval __LOCAL_ACTIONS="\$$SETUP_OBJ"
 	local ACTIONS="$__LOCAL_ACTIONS"
 	local IFS="$LINEFEED"
 	for ACTION in $ACTIONS; do
-		Exec "$ACTION"
+		Exec $SETUP_OBJ "$ACTION"
 		if [ $? -ne 0 ]; then
 			local IFS="$LINEFEED"
 			for UNWIND_ACTION in $UNWIND_ACTIONS; do
-				Revert "$UNWIND_ACTION"
+				Revert $SETUP_OBJ "$UNWIND_ACTION"
 			done
 			return 1
 		fi
@@ -55,19 +109,20 @@ Install() {
 }
 
 Uninstall() {
-	eval __LOCAL_ACTIONS="\$$1"
+	local SETUP_OBJ=$1
+	eval __LOCAL_ACTIONS="\$$SETUP_OBJ"
 	local ACTIONS="$__LOCAL_ACTIONS"
 	local IFS="$LINEFEED"
 	for ACTION in $ACTIONS; do
 		local UNWIND_ACTIONS="${ACTION}${LINEFEED}${UNWIND_ACTIONS}"
 	done
 	for UNWIND_ACTION in $UNWIND_ACTIONS; do
-		Revert "$UNWIND_ACTION"
+		Revert $SETUP_OBJ "$UNWIND_ACTION"
 	done
 }
 
 AddAction() {
-	V=$1
+	local V=$1
 	shift
 	eval __LOCAL_OLDVAL="\$$V"
 	[ -z "$__LOCAL_OLDVAL" ] || eval $V="\$${V}\"$LINEFEED\""
